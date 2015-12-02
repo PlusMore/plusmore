b876f092e19155da548697de79cd3031ed3e1f9f
Dan Voinea <dan.voinea@gmail.com>
2015-12-02 19:55:28 +0200

    fixed knowcross bugs

--- knowcross/server/functions.js
+++ knowcross/server/functions.js
@@ -2,132 +2,131 @@ baseURL = Meteor.settings.private.baseURL;
 loginOpts = Meteor.settings.private.loginOpts;
 hotelID = Meteor.settings.private.hotelID;
 
-urlToJSON = function(url){
-    var tabletojson = Meteor.npmRequire('tabletojson');
-    var html = Meteor.http.call("GET", url).content;
-    return tabletojson.convert(html);
+urlToJSON = function(url) {
+  var tabletojson = Meteor.npmRequire('tabletojson');
+  var html = Meteor.http.call("GET", url).content;
+  return tabletojson.convert(html);
 }
 
 theMarkServiceType = function(serviceType) {
-    switch (serviceType.type) {
-        case 'bellService':
-            return "812"; //Pick up luggages
-            break;
-        case 'houseKeeping':
-            switch (serviceType.options.HouseKeepingRequest) {
-                case 'Room Cleaning':
-                    return "485"; 
-                    break;          
-                case 'Turndown':
-                    return "537"; 
-                    break;          
-                case 'Bath amenities':
-                    return "678"; 
-                    break;          
-                case 'Rollaway':
-                    return "751";
-                    break;          
-                case 'Mouthwash':
-                    return "000";
-                    break;          
-                case 'Deoderant':
-                    return "000";
-                    break;          
-                case 'Pillow (feather)':
-                    return "629";
-                    break;   
-                case 'Pillow (foam)':
-                    return "626"; 
-                    break;          
-                case 'Slippers':
-                    return "505"; 
-                    break;          
-                case 'Towels':
-                    return "714"; 
-                    break;          
-                case 'Additional sheets':
-                    return "557";
-                    break;          
-                case 'Laundry Pickup (standard)':
-                    return "448";
-                    break;          
-                case 'Laundry Pickup (express)':
-                    return "453";
-                    break;          
-                case 'Laundry Delivery (standard)':
-                    return "000";
-                    break;   
-                case 'Laundry Delivery (express)':
-                    return "000"; 
-                    break;          
-                case 'Blanket':
-                    return "538"; 
-                    break;          
-                case 'Bathrobe':
-                    return "646"; 
-                    break;          
-                case 'Crib':
-                    return "605";
-                    break;          
-                case 'Iron & Ironing Board':
-                    return "566";
-                    break;          
-                case 'Steamer':
-                    return "000";
-                    break;          
-                case 'Shaving Kit':
-                    return "589";
-                    break;     
-                case 'Other':
-                    return "899";
-                    break;                       
-                default:
-                    console.log(serviceType.type.options.HouseKeepingRequest);
-                    return "485"; //Room to clean  
-            }
-            break;
-        case 'maintenance':
-            switch (serviceType.options.MaintenanceRequest) {
-                case 'Air Conditioning & Heating':
-                    return "139"; 
-                    break;          
-                case 'In Room Safe':
-                    return "888"; 
-                    break;          
-                case 'Light Bulb':
-                    return "52"; 
-                    break;          
-                case 'Shower':
-                    return "84";
-                    break;          
-                case 'Sink':
-                    return "354";
-                    break;          
-                case 'Toilet':
-                    return "401";
-                    break;          
-                case 'Other':
-                    return "294";
-                    break;          
-                default:
-                    console.log(serviceType.type.options.HouseKeepingRequest);
-                    return "294";
-            }
-            break;          
-        case 'wakeUpCall':
-            return "1"; // Courtesy call to guest //what is the proper service?
-            break;
-        case 'transportation':   
-            return "45"; //Taxi service
-            break;
-        case 'valetServices':
-            return "899"; // Other guest service center//what is the proper service?
-            break;
+  switch (serviceType.type) {
+    case 'bellService':
+      return "26816"; //Pick up luggages
+      break;
+    case 'houseKeeping':
+      switch (serviceType.options.HouseKeepingRequest) {
+        case 'Room Cleaning':
+          return "25152";
+          break;
+        case 'Turndown':
+          return "25170";
+          break;
+        case 'Bath amenities':
+          return "678"; // ??
+          break;
+        case 'Rollaway':
+          return "25070";
+          break;
+        case 'Mouthwash':
+          return "25056";
+          break;
+        case 'Deoderant':
+          return "25012"; //male - female ??
+          break;
+        case 'Pillow (feather)':
+          return "25025";
+          break;
+        case 'Pillow (foam)':
+          return "25030";
+          break;
+        case 'Slippers':
+          return "25087";
+          break;
+        case 'Towels':
+          return "25244";
+          break;
+        case 'Additional sheets':
+          return "25235"; // ?? type of room?
+          break;
+        case 'Laundry Pickup (standard)':
+          return "25196";
+          break;
+        case 'Laundry Pickup (express)':
+          return "25196"; //???
+          break;
+        case 'Laundry Delivery (standard)':
+          return "25195";
+          break;
+        case 'Laundry Delivery (express)':
+          return "25195"; //??
+          break;
+        case 'Blanket':
+          return "24986"; // ?? type of room?
+          break;
+        case 'Bathrobe':
+          return "24983";
+          break;
+        case 'Crib':
+          return "25007";
+          break;
+        case 'Iron & Ironing Board':
+          return "25043";
+          break;
+        case 'Steamer':
+          return "25186";
+          break;
+        case 'Shaving Kit':
+          return "25068";
+          break;
+        case 'Other':
+          return "25191";
+          break;
         default:
-            console.log('The type of service requested has not been configured');
-            throw new Meteor.Error(500, 'Service type is not configured', serviceRequest);
-            break;
-    }
+          console.log(serviceType.type.options.HouseKeepingRequest);
+          return "25152"; //Room to clean
+      }
+      break;
+    case 'maintenance':
+      switch (serviceType.options.MaintenanceRequest) {
+        case 'Air Conditioning & Heating':
+          return "25121";
+          break;
+        case 'In Room Safe':
+          return "888";
+          break;
+        case 'Light Bulb':
+          return "52";
+          break;
+        case 'Shower':
+          return "84";
+          break;
+        case 'Sink':
+          return "354";
+          break;
+        case 'Toilet':
+          return "401";
+          break;
+        case 'Other':
+          return "294";
+          break;
+        default:
+          console.log(serviceType.type.options.HouseKeepingRequest);
+          return "294";
+      }
+      break;
+    case 'wakeUpCall':
+      return "26721"; // Courtesy call to guest //what is the proper service?
+      break;
+    case 'transportation':
+      return "45"; //Taxi service
+      break;
+    case 'valetServices':
+      return "899"; // Other guest service center//what is the proper service?
+      break;
+    default:
+      console.log('The type of service requested has not been configured');
+      throw new Meteor.Error(500, 'Service type is not configured',
+        serviceRequest);
+      break;
+  }
 }
-
-
--- knowcross/server/startup.js
+++ knowcross/server/startup.js
@@ -1,33 +1,39 @@
-Meteor.startup(function () {
-    
-    if (Rooms.find({'hotelId':hotelID,imported:true}).count() === 0) {
-        // Import all rooms if the imported count is 0.    
-        console.log("IMPORTING ROOM LIST FOR hotel "+hotelID);
-        
-        var roomList=urlToJSON(baseURL+'LIST_LOCATIONS'+loginOpts);
+Meteor.startup(function() {
 
-        _.each(roomList[0], function(value, key) {
-            if (key!==0) {
-                var room = value[0];
+  if (Rooms.find({
+      'hotelId': hotelID,
+      imported: true
+    }).count() === 0) {
+    // Import all rooms if the imported count is 0.
+    console.log("IMPORTING ROOM LIST FOR hotel " + hotelID);
 
-                if (Rooms.find({
-                    hotelId: hotelID,
-                    name: room
-                }).count() === 0) {
-                    // Only imported if not existing
-                    Rooms.insert({
-                        hotelId: hotelID,
-                        name: room,
-                        tritonRoomId: value[1],
-                        imported: true
-                    });
-                    
-                }
-            }
-        });
+    // console.log(baseURL + 'LIST_LOCATIONS' + loginOpts);
 
-    }
+    var roomList = urlToJSON(baseURL + 'LIST_LOCATIONS' + loginOpts);
 
+    //     console.log(roomList);
+
+    _.each(roomList[0], function(value, key) {
+      if (key !== 0) {
+        var room = value[0];
+
+        // console.log(value);
+
+
+        if (Rooms.find({
+            hotelId: hotelID,
+            name: room
+          }).count() === 0) {
+          // Only imported if not existing
+          Rooms.insert({
+            hotelId: hotelID,
+            name: room,
+            tritonRoomId: value[1],
+            imported: true
+          });
+
+        }
+      }
+    });
+  }
 });
-
-
